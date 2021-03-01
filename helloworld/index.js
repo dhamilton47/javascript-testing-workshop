a29590331b32f981aa88db0767470c255148f59e
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2021-03-01 02:40:28 -0500

    Update index.js

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,6 +1,15 @@
-const program = require('commander')
+const program = require('commander');
+const { hello } = require('./hello');
 
-program.option('-n, --name <string>', 'what is your name')
-program.parse(process.argv)
+program.option('-n, --name <string>', 'what is your name');
+program.parse(process.argv);
 
-console.log(program.name)
+// Application below
+
+const run = name => {
+  console.log(hello(name));
+};
+ 
+if(program.name != '') {
+  run(program.name);
+}
